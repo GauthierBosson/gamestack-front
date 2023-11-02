37fb3cd8afd8087bd5fa31422d9adee614a2b654
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-11-02 16:59:48 +0100

    Added auth structure

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,17 +1,42 @@
-import {Authenticator} from 'remix-auth';
+import {Authenticator, AuthorizationError} from 'remix-auth';
+import type {User} from '~/services/session.server';
 import {sessionStorage} from '~/services/session.server';
 import {FormStrategy} from 'remix-auth-form';
+import {db} from '~/database/database';
 
-type User = {
-  email: string;
-  password: string;
-};
+export const authenticator = new Authenticator<User | Error>(sessionStorage);
 
-export const authenticator = new Authenticator<User>(sessionStorage);
+authenticator.use(
+  new FormStrategy(async ({form}) => {
+    const email = form.get('email') as string;
+    const password = form.get('password') as string;
 
-export const formStrategy = new FormStrategy(async ({form}) => {
-  const email = form.get('email');
-  const password = form.get('password');
+    if (!email || email?.length === 0)
+      throw new AuthorizationError('Bad Credentials: Email is required');
+    if (typeof email !== 'string')
+      throw new AuthorizationError('Bad Credentials: Email must be a string');
 
-  const user;
-});
+    if (!password || password?.length === 0)
+      throw new AuthorizationError('Bad Credentials: Password is required');
+    if (typeof password !== 'string')
+      throw new AuthorizationError(
+        'Bad Credentials: Password must be a string',
+      );
+
+    const user = await db
+      .selectFrom('users')
+      .select(['email', 'username', 'password'])
+      .where('email', '=', email)
+      .executeTakeFirst();
+
+    if (!user) throw new AuthorizationError('Bad Credentials: User not found');
+
+    // TODO: Implement password hashing
+
+    return {
+      email: user.email,
+      username: user.username,
+    };
+  }),
+  'user-pass',
+);
--- app/services/session.server.ts
+++ app/services/session.server.ts
@@ -1,11 +1,12 @@
 import {createCookieSessionStorage} from '@remix-run/node';
+import type {Users} from 'kysely-codegen';
 
 if (!process.env.SESSION_SECRET) {
   throw new Error('SESSION_SECRET is not set');
 }
 export const sessionStorage = createCookieSessionStorage({
   cookie: {
-    name: 'gamestack',
+    name: '_gamestack',
     sameSite: 'lax',
     path: '/',
     httpOnly: true,
@@ -13,5 +14,7 @@ export const sessionStorage = createCookieSessionStorage({
     secure: process.env.NODE_ENV === 'production',
   },
 });
+
+export type User = Pick<Users, 'username' | 'email'>;
 
 export const {getSession, commitSession, destroySession} = sessionStorage;
