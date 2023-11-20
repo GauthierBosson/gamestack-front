5cda45513657579be26a36951809c847fb482921
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-11-20 14:27:33 +0100

    deleted netlify conf

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,7 @@
 import {flatRoutes} from 'remix-flat-routes';
-import {config} from '@netlify/remix-adapter';
 
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ...(process.env.NODE_ENV === 'production' ? config : undefined),
   ignoredRouteFiles: ['**/.*'],
   /*routes: async (defineRoutes) => {
     return flatRoutes('routes', defineRoutes);
